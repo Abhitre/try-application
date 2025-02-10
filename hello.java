034b33dfb5c013844f0370f0d63b97624f92bbb3
Abhishek Ranjan <90136874+Abhitre@users.noreply.github.com>
2025-02-10 16:31:11 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class hello {
+    public static void main(String[] args) {
+        
+        String name = "Abhishek Ranjan";
+        
+       
+        System.out.println("My name is " + name);
+    }
+}
